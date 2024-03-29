a12ec81b37dc14b63e27f3e7a9fa23cebb0d461f
Erez <erezur5@gmail.com>
2024-03-29 17:51:24 +0300

    finished homework

--- python_homework_28-3/event_organizer_app.py
+++ python_homework_28-3/event_organizer_app.py
@@ -1,8 +1,3 @@
-events1 = {
-    {"event_name":"name",
-    "location_name":"location",
-    "date_of_event":"date"}
-}
 
 events = {}
 # Functions
@@ -14,16 +9,35 @@ def my_decorator(func):
 
 @my_decorator
 def run():
-    user_date_input = input("""Enter the date of your event in the following format
-`DD-MM-YYYY HH:MM`:\n""") # 27-07-1999 12:15 copy paste to input
-    if user_date_input:
-        format_checker = formatter(user_date_input)
-        if format_checker is True:
-            user_event_name_input = input("""Please enter event name:\n""")
-            user_location_name_input = input("""Please enter even location name:\n""")
-            events["event_name"] = user_event_name_input
-            print(events)
+    while True:
+        start = input("""Enter 'exit' to exit the program or anything else to continue:
+""")
+        if start.lower() == 'exit':
+                break
+        user_date_input = input("""Enter the date of your event in the following format
+    `DD-MM-YYYY HH:MM`:\n""") # 27-07-1999 12:15 copy paste to input
+        if user_date_input:
+            format_checker = formatter(user_date_input)
+            if format_checker is True:
+                user_event_name_input = input("""Please enter event name:\n""")
+                user_location_name_input = input("""Please enter event location name:\n""")
+                events[user_event_name_input] = {
+                    "event_location": user_location_name_input,
+                    "event_date": user_date_input
+                }
+                print(events)
+                add_attendee_names = add_attendee()
+                print("Final list of attendees:", add_attendee_names)
 
+                # Scheduling conflict checker
+                for event_name, event_details in events.items():
+                    if event_name != user_event_name_input:
+                        other_event_date = event_details.get("event_date")
+                        if other_event_date == user_date_input:
+                            print(f"Scheduling conflict: The event {event_name} coincides with another event at the same date and time.")
+                            break
+                else:
+                    print("No scheduling conflict.")
 
 
 
@@ -62,6 +76,43 @@ def formatter(data:str):
     else:
         print("Make sure to add the date in the right format")
         return False
+
+
+def add_attendee(attendee_list: list = []):
+    """
+    This function gets a list by default and lets you add, remove, and search for participants in an event where the limit is 50.
+    This function returns a list of all the participants.
+
+    """
+    while len(attendee_list) < 50:
+        action = input(
+            "Enter 'add' to add an attendee, 'remove' to remove an attendee, 'search' to search for an attendee, or 'done' to finish: ")
+        if action.lower() == 'done':
+            return attendee_list
+        elif action.lower() == 'add':
+            name = input("Enter attendee name: ")
+            attendee_list.append(name)
+            print("Current list of attendees:", attendee_list)
+        elif action.lower() == 'remove':
+            name = input("Enter attendee name to remove: ")
+            if name in attendee_list:
+                attendee_list.remove(name)
+                print("Removed", name)
+                print("Current list of attendees:", attendee_list)
+            else:
+                print(name, "not found in the list of attendees.")
+        elif action.lower() == 'search':
+            name = input("Enter attendee name to search: ")
+            if name in attendee_list:
+                print(name, "is attending the event.")
+            else:
+                print(name, "is not attending the event.")
+        else:
+            print("Invalid action. Please enter 'add', 'remove', 'search', or 'done'.")
+
+    if len(attendee_list) == 50:
+        print("The maximum limit of 50 attendees has been reached.")
+
 
 # Main
 run()
